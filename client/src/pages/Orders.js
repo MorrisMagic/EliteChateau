6f880e5b48792993bb617db7cf1bccc682449dff
youssefhabbachixxx <youssefhabbachi10@gmail.com>
2024-11-13 12:22:34 +0100

    v4

--- client/src/pages/Orders.js
+++ client/src/pages/Orders.js
@@ -1,10 +1,46 @@
 import React, { useContext } from "react";
 import { ShopContext } from "../context/ShopContext";
+import Title from "../components/Title";
 
 function Orders() {
   const { products, currency } = useContext(ShopContext);
 
-  return <div className="border-t pt-16"></div>;
+  return (
+    <div className="border-t pt-16">
+      <div className="text-2xl">
+        <Title text1={"MY"} text2={"ORDERS"} />
+      </div>
+      <div>
+        {products.slice(1, 4).map((item, index) => (
+          <div
+            className="py-4 border-t border-b text-gray-700 flex flex-col md:flex-row md:items-center md:justify-between gap-4"
+            key={index}
+          >
+            <div className="flex items-center gap-6 text-sm">
+              <img src={products?.image[0]} className="w-16 sm:w-20" alt="" />
+              <div>
+                <p className="sm:text-base font-medium">{item.name}</p>
+                <div className="flex items-center gap-3 mt-2 text-base text-gray-700">
+                  <p>
+                    {currency}
+                    {item.price}
+                  </p>
+                  <p>
+                    {currency}
+                    {item.price}
+                    <p>
+                      {currency}
+                      {item.price}
+                    </p>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default Orders;
